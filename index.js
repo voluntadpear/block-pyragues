4c2a85ffc4f5bfbe467fbdd36f492431209948ae
Guillermo Peralta Scura <gperaltascura@gmail.com>
2017-04-03 11:21:04 -0400

    Prettier script

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Twit = require("twit");
 const FS = require("fs");
-const config = require('./config');
+const config = require("./config");
 
 var T = new Twit({
- consumer_key: config.consumer_key,
+  consumer_key: config.consumer_key,
   consumer_secret: config.consumer_secret,
   access_token: config.access_token,
   access_token_secret: config.access_token_secret,
@@ -26,19 +26,18 @@ function getAllPyragues() {
 getAllPyragues().then(() => {
   console.log("Fin de colecta de pyragues");
   let idsMap = pyragues.map(p => p.id_str);
-  FS.writeFile("pyragues.csv", idsMap.join('\r\n'), err => {
+  FS.writeFile("pyragues.csv", idsMap.join("\r\n"), err => {
     if (err) {
       console.error(`Error al generar CSV: ${err}`);
       return;
     }
   });
-  FS.writeFile("pyragues.json", JSON.stringify(pyragues),
-  err => {
+  FS.writeFile("pyragues.json", JSON.stringify(pyragues), err => {
     if (err) {
       console.error(`Error al generar JSON: ${err}`);
     }
   });
-  console.log(`Registros encontrados: ${pyragues.length}`)
+  console.log(`Registros encontrados: ${pyragues.length}`);
 });
 
 function getPyragues(cursor) {
@@ -57,15 +56,14 @@ function getPyragues(cursor) {
           return;
         }
 
-        let pyragues = data.users.filter(
-          u => {
-            const fecha = new Date(u.created_at);
-            return u.profile_image_url == "http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png" &&
-              u.followers_count < 100 &&
-              u.friends_count < 150 &&
-              fecha > new Date(2017, 3, 1);
-          }
-        );
+        let pyragues = data.users.filter(u => {
+          const fecha = new Date(u.created_at);
+          return u.profile_image_url ==
+            "http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png" &&
+            u.followers_count < 100 &&
+            u.friends_count < 150 &&
+            fecha > new Date(2017, 3, 1);
+        });
         let nextCursor = data.next_cursor_str;
         resolve({ pyragues, nextCursor });
       }
